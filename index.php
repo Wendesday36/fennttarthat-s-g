43a74d33c6b640455fcd5fbac92afba276a8955c
Wendesday36 <bertazoeanna@gmail.com>
2023-03-22 11:49:46 +0100

    alap

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-phpinfo();
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fenntarthatóság</title>
+
+</head>
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>
