620b7abc3383e260eb9fd15cac70d9a10630bdc7
Wendesday36 <bertazoeanna@gmail.com>
2023-03-22 12:12:52 +0100

    tablazat

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fenntarthatóság</title>
-
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <main>
         <div><img src="./kepek/Képernyőkép 2023-03-22 114412.png" alt="torony"></div>
+        <h1>A fenntarthatóság szintjei</h1>
     </main>
     <?php
     $elsoSzint = "ökoszisztéma";
@@ -23,8 +24,14 @@
     echo"<li>$masoidkSzint</li>";
     echo"<li>$harmadikSzint</li>";
     echo"<li>$negyedikSzint</li>";
-    echo"</ul>"
+    echo"</ul>";
+    echo"<tr>";
+    echo"<th>";"A megújuló erőforrások fogyasztása";"</th>";
+    echo"<th>";"A környezet helyzete";"</th>";
+    echo"<th>";"Fenntarthatóság";"</th>";
+    echo"</tr>";
     ?>
+
 </body>
 
 </html>
